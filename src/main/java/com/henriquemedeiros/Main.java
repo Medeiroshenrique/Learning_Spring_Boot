492623d15efeffd0bcd7706643b8bf179a33a810
Medeiroshenrique <henrique.c.r.medeiros@gmail.com>
2024-07-16 15:28:08 -0300

    adding without serialization logic as a spring boot feature

--- src/main/java/com/henriquemedeiros/Main.java
+++ src/main/java/com/henriquemedeiros/Main.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 
 //@SpringBootApplication
 @ComponentScan(basePackages = "com.henriquemedeiros")
@@ -23,9 +26,39 @@ public class Main {
 
     @GetMapping ("/hello_word")
     public GreetResponse greet(){
-
-        return new GreetResponse("Hello World!");
+        GreetResponse response = new GreetResponse("Hello World!", List.of("C#","Java", "Python"), new Person("Iram Cesar",55, 77777));
+        return response;
     }
 
-    record  GreetResponse(String greet){}
+    record  GreetResponse(String greet, List<String> favoriteProgrammingLanguages, Person person){}
+
+    record Person(String name, int age, double savings){}
+ //   class GreetResponse{
+   //     private final String greet;
+//
+  //      GreetResponse(String greet) {
+    //        this.greet = greet;
+      //  }
+//
+  //      public String getGreet() {
+    //        return greet;
+      //      }
+        //@Override
+        //public String toString() {
+          //  return super.toString();
+        //}
+//
+  //      @Override
+    //    public boolean equals(Object o) {
+      //      if (this == o) return true;
+        ////    if (o == null || getClass() != o.getClass()) return false;
+            //GreetResponse that = (GreetResponse) o;
+            //return Objects.equals(greet, that.greet);
+        //}
+
+        //@Override
+        //public int hashCode() {
+          //  return Objects.hashCode(greet);
+       // }
+    //}
 }
